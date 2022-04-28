e062ee9c1770a43bd13ca58ede48c2284a3a5278
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-04-29 06:40:47 +0800

    Update build-list.sh

--- script/build-list.sh
+++ script/build-list.sh
@@ -8,4 +8,5 @@ bash ./*/prepare-upstream.sh
 cd script
 #php make-addr.php
 #php ./adguard-extend.php ../anti-ad-adguard.txt
-php ./easylist-extend.php tmp/base-src-easylist.txt
+php ./adguard-extend.php tmp/base-src-easylist.txt
+#php ./easylist-extend.php tmp/base-src-easylist.txt
