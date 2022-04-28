a4326f2636963a82615b1e45594783089d227326
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-04-28 23:50:13 +0800

    Update build-list.sh

--- script/build-list.sh
+++ script/build-list.sh
@@ -3,7 +3,7 @@
 #source /etc/profile
 
 #cd $(cd "$(dirname "$0")";pwd)
-./*/prepare-upstream.sh
+sudo ./*/prepare-upstream.sh
 cd script
 php make-addr.php
 echo
@@ -12,6 +12,6 @@ echo "!Version: $(date "+%Y%m%d%H%M%S%N")" >> ../anti-ad-adguard.txt
 echo "!Homepage: https://anti-ad.net/" >> ../anti-ad-adguard.txt
 echo "!!!note! If you are using \"AdGuard Home\", please load https://anti-ad.net/easylist.txt" >> ../anti-ad-adguard.txt
 echo "!Total lines: 00000" >> ../anti-ad-adguard.txt
-grep -vE '^!' ../anti-ad-easylist.txt >> ../anti-ad-adguard.txt
-php ./adguard-extend.php ../anti-ad-adguard.txt
+grep -v '^!' ../anti-ad-easylist.txt >> ../anti-ad-adguard.txt
+#php ./adguard-extend.php ../anti-ad-adguard.txt
 php ./easylist-extend.php ../anti-ad-easylist.txt
